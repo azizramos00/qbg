07a633aa07ee402c24a7f9155e2e0314d63e36f7
azizramos00 <azizramos00@gmail.com>
2026-05-19 11:29:13 -0400

    adjusting call to action cards

--- patterns/call-to-action-card.php
+++ patterns/call-to-action-card.php
@@ -11,8 +11,8 @@ defined( 'ABSPATH' ) || exit;
 
 $qbb_cta_image_url = esc_url( get_template_directory_uri() . '/assets/images/cta/call-to-action-membership.jpg' );
 ?>
-<!-- wp:group {"className":"site-container\u002d\u002dfull-width","layout":{"type":"default"}} -->
-<div class="wp-block-group site-container--full-width"><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|m","left":"var:preset|spacing|m","top":"var:preset|spacing|m","bottom":"var:preset|spacing|m"}},"color":{"background":"#faf7e1"},"border":{"radius":{"topLeft":"24px","topRight":"24px","bottomLeft":"24px","bottomRight":"24px"}}},"layout":{"type":"grid","columnCount":12,"minimumColumnWidth":null}} -->
+<!-- wp:group {"className":"site-container\u002d\u002dfull-width callAction\u002d\u002dcard","style":{"spacing":{"margin":{"top":"var(--qbb-space-l)","bottom":"var(--qbb-space-m)"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group site-container--full-width callAction--card" style="margin-top:var(--qbb-space-l);margin-bottom:var(--qbb-space-m)"><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|m","left":"var:preset|spacing|m","top":"var:preset|spacing|m","bottom":"var:preset|spacing|m"}},"color":{"background":"#faf7e1"},"border":{"radius":{"topLeft":"24px","topRight":"24px","bottomLeft":"24px","bottomRight":"24px"}}},"layout":{"type":"grid","columnCount":12,"minimumColumnWidth":null}} -->
 <div class="wp-block-group has-background" style="border-top-left-radius:24px;border-top-right-radius:24px;border-bottom-left-radius:24px;border-bottom-right-radius:24px;background-color:#faf7e1;padding-top:var(--wp--preset--spacing--m);padding-right:var(--wp--preset--spacing--m);padding-bottom:var(--wp--preset--spacing--m);padding-left:var(--wp--preset--spacing--m)"><!-- wp:group {"style":{"layout":{"columnSpan":5}},"layout":{"type":"default"}} -->
 <div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|peony"}}}},"textColor":"peony"} -->
 <h2 class="wp-block-heading has-peony-color has-text-color has-link-color">Become a member</h2>
